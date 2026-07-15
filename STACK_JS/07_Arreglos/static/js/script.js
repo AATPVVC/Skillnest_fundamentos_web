10323db262071cc98d95de12d4d03636d90f90a9
AATPVVC <angelalvarez@liceovvh.cl>
2026-07-15 09:27:55 -0400

    ok

--- STACK_JS/07_Arreglos/static/js/script.js
+++ STACK_JS/07_Arreglos/static/js/script.js
@@ -1,2 +1,12 @@
 console.log("Conexion exitosa...")
 
+//Variables a lista 
+let nombre1 = "Ana";
+let nombre2 = "Pedro";
+let nombre3 = "María";
+let nombre4 = "Carlos";
+//Transformar en array y mostrar
+let Nombres = ["Ana", "Pedro", "Maria", "Carlos"];
+
+//Mostrar a Maria en una alerta
+alert(`${Maria}`);
